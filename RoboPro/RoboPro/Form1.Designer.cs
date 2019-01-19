cf2fc87ba9bf56b56dae162530f09c09bc7ca420
izsoandras <izso.andras.zsolt@gmail.com>
2019-01-19 19:30:19 +0100

    init commit

---
+++ RoboPro/RoboPro/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace RoboPro
+{
+    partial class robotClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.pbCameraView = new System.Windows.Forms.PictureBox();
+            this.tbConsole = new System.Windows.Forms.TextBox();
+            this.labelWSAddress = new System.Windows.Forms.Label();
+            this.btnWSConnect = new System.Windows.Forms.Button();
+            this.tbWSAddress = new System.Windows.Forms.TextBox();
+            this.labelCamAddress = new System.Windows.Forms.Label();
+            this.tbCamAddress = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.labelWSSend = new System.Windows.Forms.Label();
+            this.tbWSSend = new System.Windows.Forms.TextBox();
+            this.btnWSSend = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCameraView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnWSSend);
+            this.splitContainer1.Panel2.Controls.Add(this.tbWSSend);
+            this.splitContainer1.Panel2.Controls.Add(this.labelWSSend);
+            this.splitContainer1.Panel2.Controls.Add(this.labelWSAddress);
+            this.splitContainer1.Panel2.Controls.Add(this.btnWSConnect);
+            this.splitContainer1.Panel2.Controls.Add(this.tbWSAddress);
+            this.splitContainer1.Panel2.Controls.Add(this.labelCamAddress);
+            this.splitContainer1.Panel2.Controls.Add(this.tbCamAddress);
+            this.splitContainer1.Panel2.Controls.Add(this.btnConnect);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 598;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.pbCameraView);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.tbConsole);
+            this.splitContainer2.Size = new System.Drawing.Size(598, 450);
+            this.splitContainer2.SplitterDistance = 319;
+            this.splitContainer2.TabIndex = 1;
+            // 
+            // pbCameraView
+            // 
+            this.pbCameraView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbCameraView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbCameraView.Location = new System.Drawing.Point(12, 12);
+            this.pbCameraView.Name = "pbCameraView";
+            this.pbCameraView.Size = new System.Drawing.Size(579, 300);
+            this.pbCameraView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbCameraView.TabIndex = 1;
+            this.pbCameraView.TabStop = false;
+            // 
+            // tbConsole
+            // 
+            this.tbConsole.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbConsole.Location = new System.Drawing.Point(12, 3);
+            this.tbConsole.Multiline = true;
+            this.tbConsole.Name = "tbConsole";
+            this.tbConsole.ReadOnly = true;
+            this.tbConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbConsole.Size = new System.Drawing.Size(579, 108);
+            this.tbConsole.TabIndex = 0;
+            // 
+            // labelWSAddress
+            // 
+            this.labelWSAddress.AutoSize = true;
+            this.labelWSAddress.Location = new System.Drawing.Point(3, 88);
+            this.labelWSAddress.Name = "labelWSAddress";
+            this.labelWSAddress.Size = new System.Drawing.Size(95, 13);
+            this.labelWSAddress.TabIndex = 6;
+            this.labelWSAddress.Text = "Web Socket URL:";
+            // 
+            // btnWSConnect
+            // 
+            this.btnWSConnect.Location = new System.Drawing.Point(116, 130);
+            this.btnWSConnect.Name = "btnWSConnect";
+            this.btnWSConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnWSConnect.TabIndex = 5;
+            this.btnWSConnect.Text = "Connect";
+            this.btnWSConnect.UseVisualStyleBackColor = true;
+            this.btnWSConnect.Click += new System.EventHandler(this.btnWSConnect_Click);
+            // 
+            // tbWSAddress
+            // 
+            this.tbWSAddress.Location = new System.Drawing.Point(6, 104);
+            this.tbWSAddress.Name = "tbWSAddress";
+            this.tbWSAddress.Size = new System.Drawing.Size(185, 20);
+            this.tbWSAddress.TabIndex = 4;
+            this.tbWSAddress.Text = "ws://";
+            // 
+            // labelCamAddress
+            // 
+            this.labelCamAddress.AutoSize = true;
+            this.labelCamAddress.Location = new System.Drawing.Point(3, 7);
+            this.labelCamAddress.Name = "labelCamAddress";
+            this.labelCamAddress.Size = new System.Drawing.Size(68, 13);
+            this.labelCamAddress.TabIndex = 3;
+            this.labelCamAddress.Text = "Stream URL:";
+            // 
+            // tbCamAddress
+            // 
+            this.tbCamAddress.Location = new System.Drawing.Point(3, 23);
+            this.tbCamAddress.Name = "tbCamAddress";
+            this.tbCamAddress.Size = new System.Drawing.Size(188, 20);
+            this.tbCamAddress.TabIndex = 2;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(116, 49);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // labelWSSend
+            // 
+            this.labelWSSend.AutoSize = true;
+            this.labelWSSend.Location = new System.Drawing.Point(3, 166);
+            this.labelWSSend.Name = "labelWSSend";
+            this.labelWSSend.Size = new System.Drawing.Size(140, 13);
+            this.labelWSSend.TabIndex = 7;
+            this.labelWSSend.Text = "Send WebSocket message:";
+            // 
+            // tbWSSend
+            // 
+            this.tbWSSend.Enabled = false;
+            this.tbWSSend.Location = new System.Drawing.Point(6, 182);
+            this.tbWSSend.Name = "tbWSSend";
+            this.tbWSSend.Size = new System.Drawing.Size(185, 20);
+            this.tbWSSend.TabIndex = 8;
+            // 
+            // btnWSSend
+            // 
+            this.btnWSSend.Enabled = false;
+            this.btnWSSend.Location = new System.Drawing.Point(116, 208);
+            this.btnWSSend.Name = "btnWSSend";
+            this.btnWSSend.Size = new System.Drawing.Size(75, 23);
+            this.btnWSSend.TabIndex = 9;
+            this.btnWSSend.Text = "Send";
+            this.btnWSSend.UseVisualStyleBackColor = true;
+            this.btnWSSend.Click += new System.EventHandler(this.btnWSSend_Click);
+            // 
+            // robotClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "robotClientForm";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.robotClientForm_FormClosing);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCameraView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.PictureBox pbCameraView;
+        private System.Windows.Forms.TextBox tbConsole;
+        private System.Windows.Forms.Label labelCamAddress;
+        private System.Windows.Forms.TextBox tbCamAddress;
+        private System.Windows.Forms.Label labelWSAddress;
+        private System.Windows.Forms.Button btnWSConnect;
+        private System.Windows.Forms.TextBox tbWSAddress;
+        private System.Windows.Forms.Button btnWSSend;
+        private System.Windows.Forms.TextBox tbWSSend;
+        private System.Windows.Forms.Label labelWSSend;
+    }
+}
+
---
+++ RoboPro/RoboPro/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using System.IO;
+using WebSocketSharp;
+
+namespace RoboPro
+{
+    /// <summary>
+    /// The inherited class from Windows.Forms.Form, which needed to make our own
+    /// </summary>
+    /// <seealso cref="System.Windows.Forms.Form" />
+    public partial class robotClientForm : Form
+    {
+        /// <summary>
+        /// The video stream from the camera of the robot
+        /// </summary>
+        MJPEGStream videoStream;
+        /// <summary>
+        /// The logger, which logs messages to all subscribed places. 
+        /// The <c>Logger.Log</c> event is fired when the <c>Logger.LogMsg</c> method is called.
+        /// </summary>
+        Utils.Logger logger;
+        /// <summary>
+        /// The WebSocket object, which represents the connection to the robot's WebSocket server.
+        /// </summary>
+        WebSocket ws;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="robotClientForm"/> class.
+        /// After the auto generated initialization we check if there are previous values to load, and subscribe the loggers.
+        /// </summary>
+        public robotClientForm()
+        {
+            InitializeComponent();
+
+            FileInfo ipFile = new FileInfo("lastvalues.txt");
+
+            if (ipFile.Exists)
+            {
+                using (StreamReader sw = new StreamReader(ipFile.FullName))
+                {
+                    tbCamAddress.Text = sw.ReadLine();
+                    tbWSAddress.Text = sw.ReadLine();
+                }
+            }
+            logger = new Utils.Logger();
+             logger.Log += LogToConsole;
+        }
+        /// <summary>
+        /// Logs to the application's console.
+        /// </summary>
+        /// <param name="msg">The message to be logged.</param>
+        private void LogToConsole(string msg)
+        {
+            //If we aren't on the GUI thread we need to use invoke
+            if (InvokeRequired)
+            {
+                try
+                {
+                    Invoke(new Utils.LogHandler(LogToConsole), msg + "\n");
+                }
+                catch (ObjectDisposedException) { }
+            }
+            else
+            {
+                tbConsole.AppendText(msg + "\n");
+            }
+        }
+        /// <summary>
+        /// Handles the Click event of the btnConnect control.
+        /// Starts the video stream and changes the button's behaviour to disconnect.
+        /// Also saves the URL input so it can be reloaded after restart.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            //Starting the stream
+            videoStream = new MJPEGStream(tbCamAddress.Text);
+            videoStream.NewFrame += videoStream_NewFrame;
+            videoStream.Start();
+
+            //TODO: URL, stream validation
+
+            //Save the input
+            FileInfo ipFile = new FileInfo("lastvalues.txt");
+            using (StreamWriter sw = new StreamWriter(ipFile.FullName))
+            {
+                sw.WriteLine(tbCamAddress.Text);
+                sw.WriteLine(tbWSAddress.Text);
+            }
+
+            //Log the event
+            logger.LogMsg("Connected to: " + tbCamAddress.Text);
+
+            //Change the button's behaviour
+            btnConnect.Text = "Disconnect";
+            tbCamAddress.Enabled = false;
+            btnConnect.Click -= btnConnect_Click;
+            btnConnect.Click += btnDisconnect_Click;
+        }
+        /// <summary>
+        /// Handles the Click event of the btnConnect control.
+        /// This event handler is used when the streaming is in progress.
+        /// It stops the streaming, logs the event and changes the button's behaviour to connect.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            //Stop the stream
+            videoStream.Stop();
+
+            //Log the event
+            logger.LogMsg("Disconnected");
+
+            //Change the button's behaviour
+            btnConnect.Text = "Connect";
+            tbCamAddress.Enabled = true;
+            btnConnect.Click -= btnDisconnect_Click;
+            btnConnect.Click += btnConnect_Click;
+        }
+        /// <summary>
+        /// Handles the NewFrame event of the videoStream control.
+        /// It prints out the new frame on the pbCameraView
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="eventArgs">The <see cref="NewFrameEventArgs"/> instance containing the event data.</param>
+        private void videoStream_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pbCameraView.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+        /// <summary>
+        /// Handles the FormClosing event of the robotClientForm control.
+        /// It closes the costly resources if it hasn't been done yet.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="FormClosingEventArgs"/> instance containing the event data.</param>
+        private void robotClientForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(videoStream != null)
+                videoStream.Stop();
+            if (ws != null)
+                ws.Close();
+        }
+        /// <summary>
+        /// Enables/disables the controls connected to the WebSocket message sending functionality.
+        /// </summary>
+        /// <param name="enabled">if set to <c>true</c> [enabled].</param>
+        private delegate void SetEnableWSMessageDelegate(bool enabled);
+        private void SetEnableWSMessage(bool enabled)
+        {
+            //If we aren't on the GUI thread we need to use invoke
+            if (InvokeRequired)
+            {
+                Invoke(new SetEnableWSMessageDelegate(SetEnableWSMessage), enabled);
+            }
+            else
+            {
+                btnWSSend.Enabled = enabled;
+                tbWSSend.Enabled = enabled;
+            }
+        }
+        /// <summary>
+        /// Handles the Click event of the btnWSConnect control.
+        /// Creates the instance of the <see cref="WebSocket"/> class and sets the event listeners for it.
+        /// 
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void btnWSConnect_Click(object sender, EventArgs e)
+        {
+            //Instantiate the WebSocket object and subscribe the listeners
+            ws = new WebSocket(tbWSAddress.Text);
+            ws.OnOpen += (opensender, evt) =>
+            {
+                //Log the event
+                logger.LogMsg("Connected to WebSocet server: " + tbWSAddress.Text);
+
+                //Save the input
+                FileInfo ipFile = new FileInfo("lastvalues.txt");
+                using (StreamWriter sw = new StreamWriter(ipFile.FullName))
+                {
+                    sw.WriteLine(tbCamAddress.Text);
+                    sw.WriteLine(tbWSAddress.Text);
+                }
+                //Enables the controls' for message sending
+                SetEnableWSMessage(true);
+            };
+            ws.OnError += (opensender, evt) =>
+            {
+                //Log the event
+                WebSocketSharp.ErrorEventArgs errorarg = (WebSocketSharp.ErrorEventArgs)evt;
+                logger.LogMsg("WebSocket error: " + errorarg.Message);
+            };
+            ws.OnClose += (opensender, evt) =>
+            {
+                //Log the event
+                logger.LogMsg("Disconnected from WebSocet server");
+                SetEnableWSMessage(false);
+            };
+            ws.OnMessage += (opensender, evt) =>
+            {
+                //Log the event
+                WebSocketSharp.MessageEventArgs msge = (WebSocketSharp.MessageEventArgs)evt;
+                logger.LogMsg("WebSocket message arrived: " + msge.Data);
+            };
+            ws.Connect();
+
+
+            // TODO put it in the OnOpen event
+            //Change the button's behaviour to disconnect
+            btnWSConnect.Text = "Disconnect";
+            btnWSConnect.Click -= btnWSConnect_Click;
+            btnWSConnect.Click += btnWSDisconnect_Click;
+            tbWSAddress.Enabled = false;
+        }
+        /// <summary>
+        /// Handles the Click event of the btnWSDisconnect control.
+        /// Close the WebSocket connection and change the button's behaviour to connect
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void btnWSDisconnect_Click(object sender, EventArgs e)
+        {
+            //Close the connection
+            ws.Close();
+
+            //Change the button's behaviour
+            btnWSConnect.Text = "Connect";
+            btnWSConnect.Click -= btnWSDisconnect_Click;
+            btnWSConnect.Click += btnWSConnect_Click;
+            tbWSAddress.Enabled = true;
+        }
+        /// <summary>
+        /// Handles the Click event of the btnWSSend control.
+        /// Sends the message typed in the tbWSSend textbox and logs the event.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void btnWSSend_Click(object sender, EventArgs e)
+        {
+            //Sends the message
+            ws.Send(tbWSSend.Text);
+            //Logs the event.
+            logger.LogMsg("WebSocket message sent: " + tbWSSend.Text);
+        }
+    }
+}
